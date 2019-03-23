b9cc13dd66db472737d083c52cfc95fedfa07b3f
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-03-23 16:15:26 -0300

    Evolves transformer examples

--- transformer.pl
+++ transformer.pl
@@ -4,6 +4,10 @@ start :-
 	load_graph("D:/Workspace/MitrasTest/Graph.pl"),
 	find_class_by_name("Employee", Employee),
 	find_type_by_name("String", Type),
-	add_attribute(Employee, "CPF", Type, Attribute),
-	edge(X, unsynchronized, X),
-	write(X).
+	add_attribute(Employee, "CPF", Type, Attribute), !,
+	add_method(Employee, "kill", void, ['public'], [
+		parameter([final], 'String', person),
+		parameter([], 'boolean', old)
+	]),
+	edge(Attribute, unsynchronized, Attribute),
+	write(Attribute).
