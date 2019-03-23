52f08372849e1cdcfb677d2ae9d583804f98f0dd
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-03-23 16:14:11 -0300

    Implements methods and parameters transformations

--- model/method.pl
+++ model/method.pl
@@ -5,12 +5,17 @@
 	get_method_modifiers/2,
 	get_method_return/2,
 	get_method_name/2,
-	get_method_parameters/2
+	get_method_parameters/2,
+	add_method/6
 ]).
 
 :- use_module(graph, [edge/3, vertex/2]).
 :- use_module(common, [is_type/1, get_name/2, get_modifiers/2]).
 :- use_module(class, [is_class/1, find_class/2]).
+:- use_module(modifier, [is_modifier/1]).
+:- use_module(parameter, [add_parameter/5]).
+:- use_module('../representation/qualified_name').
+:- use_module('../arrays').
 
 % Assertion Theorems
 is_method(Method) :-
@@ -54,10 +59,44 @@ get_method_parameters(Method, Parameters) :-
 	is_method(Method),
 	findall(Parameter, edge(Method, parameter, Parameter), Parameters).
 
-% Transformation Theorems
-can_add_method(Class, Return, Parameters) :-
-	is_class(Class),
-	is_type(Return).
+%% Transformation Theorems
+% Validation Theorems
+modifiers_are_valid([]).
+modifiers_are_valid([Modifier|Rest]) :-
+	is_modifier(Modifier),
+	modifiers_are_valid(Rest).
 
-add_method(Class, Name, Modifiers, Return, Parameters) :-
-	can_add_method(Class, Return, Parameters).
+parameters_have_different_names([], _).
+parameters_have_different_names([parameter(_, _, Name)|Rest], Parameters) :-
+	\+ has_multiple_occurrences(parameter(_, _, Name), Parameters),
+	parameters_have_different_names(Rest, Parameters).
+parameters_have_different_names(Parameters) :-
+	parameters_have_different_names(Parameters, Parameters).
+
+can_add_method(Class, Modifiers, Return, Parameters) :-
+	is_class(Class),
+	is_type(Return),
+	modifiers_are_valid(Modifiers), !,
+	parameters_have_different_names(Parameters).
+
+% Creation Theorems
+create_modifiers_edges(Method, []).
+create_modifiers_edges(Method, [Modifier|Rest]) :-
+	create_edge(Method, modifier, Modifier),
+	create_modifiers_edges(Method, Rest).
+
+create_parameters([]).
+create_parameters([parameter(Modifiers, Type, Name)|Rest]) :-
+	add_parameter(Method, Modifiers, Type, Name, _),
+	create_parameters(Rest).
+
+add_method(Class, Modifiers, Return, Name, Parameters, Method) :-
+	can_add_method(Class, Modifiers, Return, Parameters),
+	generate_qualified_name(Class, Name, Method),
+	create_vertex(method, Method),
+	create_edge(Method, unsynchronized, Method),
+	create_edge(Class, method, Method),
+	create_modifiers_edges(Method, Modifiers),
+	create_edge(Method, return, Return),
+	create_edge(Method, name, Name)
+	create_parameters(Parameters).
--- model/parameter.pl
+++ model/parameter.pl
@@ -2,12 +2,13 @@
 	find_parameter_by_name/4,
 	get_parameter_modifiers/2,
 	get_parameter_type/2,
-	get_parameter_name/2
+	get_parameter_name/2,
+	add_parameter/5
 ]).
 
 :- use_module(graph, [edge/3, vertex/2]).
 :- use_module(common, [get_name/2, get_type/2, get_modifiers/2]).
-:- use_module(method, [find_method/3]).
+:- use_module(method, [is_method/1, find_method/3]).
 
 % Assertion Theorems
 is_parameter(Parameter) :-
@@ -33,3 +34,30 @@ get_parameter_type(Parameter, Type) :-
 get_parameter_name(Parameter, Name) :-
 	is_parameter(Parameter),
 	get_name(Parameter, Name).
+
+%% Transformation Theorems
+% Validation Theorems
+modifiers_are_valid([]).
+modifiers_are_valid([Modifier|Rest]) :-
+	is_modifier(Modifier),
+	modifiers_are_valid(Rest).
+
+can_add_parameter(Method, Modifiers, Type) :-
+	is_method(Method),
+	is_type(Type),
+	modifiers_are_valid(Modifiers).
+
+% Creation Theorems
+create_modifiers_edges(Parameter, []).
+create_modifiers_edges(Parameter, [Modifier|Rest]) :-
+	create_edge(Parameter, modifier, Modifier),
+	create_modifiers_edges(Parameter, Rest).
+
+add_parameter(Method, Modifiers, Type, Name, Parameter) :-
+	can_add_parameter(Method, Modifiers, Type),
+	generate_qualified_name(Method, Name, Parameter),
+	create_vertex(parameter, Parameter),
+	create_edge(Parameter, unsynchronized, Parameter),
+	create_modifiers_edges(Parameter, Modifiers),
+	create_edge(Parameter, type, Type)
+	create_edge(Parameter, name, Name).
