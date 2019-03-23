9473e8256a5c0b0f30873e641ce5a84060108e62
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-03-23 16:12:59 -0300

    Fixes qualified_name numbering

--- representation/qualified_name.pl
+++ representation/qualified_name.pl
@@ -8,7 +8,7 @@
 is_qualified_name_available(QualifiedName) :-
 	\+ vertex(_, QualifiedName).
 
-generate_numbered_name(Name, 0, Name) :- !.
+generate_numbered_name(Name, 1, Name) :- !.
 generate_numbered_name(Name, Number, NumberedName) :-
 	atom_concat(Name, Number, NumberedName).
 
@@ -21,4 +21,4 @@ generate_qualified_name(Parent, Name, Number, QualifiedName) :-
 	generate_qualified_name(Parent, Name, NextNumber, QualifiedName).
 
 generate_qualified_name(Parent, Name, QualifiedName) :-
-	generate_qualified_name(Parent, Name, 0, QualifiedName).
+	generate_qualified_name(Parent, Name, 1, QualifiedName), !.
