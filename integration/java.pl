55cc79e9a5ee5a1f631afbb4ac15c9947efb87f1
i841748 <gabriel.nunes@sap.com>
2019-04-25 15:12:33 -0300

    Finishes implementation of Java integration

--- integration/java.pl
+++ integration/java.pl
@@ -1,16 +1,23 @@
 :- module(java, [
   invoke_java/2,
-  invoke_java/3
+  invoke_java/3,
+  inject_attribute/5,
+  inject_getter/4,
+  inject_setter/4
 ]).
 
 :- use_module(library(process)).
+:- use_module(library(http/json)).
+:- use_module('../system/console', [write_all/1]).
 
+% Java Theorems
 get_java_executable_path(Path) :-
   getenv('JAVA_HOME', Home),
   atom_concat(Home, '/bin/java', Path).
 
 invoke_java(Jar, Arguments, Timeout) :-
   get_java_executable_path(Java),
+  write_all([Java, "-jar", Jar | Arguments]),
   process_create(Java, ["-jar", Jar|Arguments], [
     stdin(std),
     stdout(std),
@@ -21,3 +28,23 @@ invoke_java(Jar, Arguments, Timeout) :-
 
 invoke_java(Jar, Arguments) :-
   invoke_java(Jar, Arguments, 120).
+
+% Injector Theorems
+get_injector_executable_path(Path) :-
+  getenv('INJECTOR_JAR', Path).
+
+invoke_injector(File, Injection, JSON) :-
+  get_injector_executable_path(Injector),
+  invoke_java(Injector, ["-s", File, "-l", "JAVA", "-i", Injection, JSON]).
+
+inject_attribute(File, Class, Modifiers, Type, Name) :-
+  atom_json_dict(JSON, _{ class: Class, modifiers: Modifiers, type: Type, name: Name }, []),
+  invoke_injector(File, "attribute", JSON).
+
+inject_getter(File, Class, Modifiers, Attribute) :-
+  atom_json_dict(JSON, _{ class: Class, modifiers: Modifiers, attribute: Attribute }, []),
+  invoke_injector(File, "getter", JSON).
+
+inject_setter(File, Class, Modifiers, Attribute) :-
+  atom_json_dict(JSON, _{ class: Class, modifiers: Modifiers, attribute: Attribute }, []),
+  invoke_injector(File, "setter", JSON).
