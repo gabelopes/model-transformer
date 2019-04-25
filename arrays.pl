e5564a4cf8a46aa4f9b812aaedb6f8421f0491ff
i841748 <gabriel.nunes@sap.com>
2019-04-25 20:01:00 -0300

    Removes useless implementations in arrays

--- arrays.pl
+++ arrays.pl
@@ -1,22 +1,11 @@
 :- module(arrays, [
   has_multiple_occurrences/2,
-  has_multiple_occurrences_del/2,
-  has_multiple_occurrences_sel/2,
   occurrences/3,
-  occurrences_del/3
 ]).
 
 has_multiple_occurrences(Element, Array) :-
   occurrences(Element, Array, Occurrences),
   Occurrences > 1.
-
-has_multiple_occurrences_del(Element, Array) :-
-  occurrences_del(Element, Array, Occurrences),
-  Occurrences > 1.
-
-has_multiple_occurrences_sel(Element, Array) :-
-  select(Element, Array, Differential),
-  member(Element, Differential).
 
 occurrences(_, [], Count, Count).
 occurrences(Element, [Head|Tail], Count, Occurrences) :-
@@ -27,9 +16,3 @@ occurrences(Element, [_|Tail], Count, Occurrences) :-
   occurrences(Element, Tail, NextCount, Occurrences).
 occurrences(Element, Array, Occurrences) :-
   occurrences(Element, Array, 0, Occurrences), !.
-
-occurrences_del(Element, Array, Occurrences) :-
-  delete(Array, Element, Differential),
-  length(Array, X),
-  length(Differential, Y),
-  Occurrences is X - Y.
