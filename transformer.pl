0aa6ae69563a54b3d36b0548e1d3fffe005c5172
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-04-26 00:02:02 -0300

    Finishes apply_transformation implementation

--- transformer.pl
+++ transformer.pl
@@ -1,12 +1,19 @@
 :- module(transformer, [
-  apply_transformation/2
+  apply_transformation/3
 ]).
 
-:- use_module(cli, [find_option/3]).
+:- use_module(model/graph, [load_graph/1, synchronize_graph/0]).
+:- use_model(model/attribute, [add_attribute/5]).
 
-specification(add_attribute, [
-  [opt('class'), shortflags(['c']), longflags(['class']), type(atom)],
-  [opt('modifiers'), shortflags(['m']), longflags(['modifiers']), type(atom)]
-]).
+add_attribute([Class, Modifiers, Type, Name]) :-
+  add_attribute(Class, Modifiers, Type, Name, _).
 
 apply_transformation(Graph, Transformation, Arguments) :-
+  string(Transformation),
+  atom_string(TransformationFunctor, Transformation), !,
+  apply_transformation(Graph, TransformationFunctor, Arguments).
+apply_transformation(Graph, Transformation, Arguments) :-
+  load_graph(Graph),
+  TransformationPredicate =.. [Transformation, Arguments], !,
+  call(TransformationPredicate),
+  synchronize_graph.
