a55dd071a2cfddae301cfbbb6a3228c91eb0d112
i841748 <gabriel.nunes@sap.com>
2019-04-26 21:39:35 -0300

    Implements add_attribute predicate to transformer

--- transformer.pl
+++ transformer.pl
@@ -2,11 +2,17 @@
   apply_transformation/3
 ]).
 
-:- use_module(model/graph, [load_graph/1, synchronize_graph/0]).
-:- use_model(model/attribute, [add_attribute/5]).
+:- use_module(model/graph, [load_graph/1, synchronize_graph/0, find_owner/2]).
+:- use_module(model/attribute, [add_attribute/5]).
+:- use_module(integration/java, [inject_attribute/5, inject_getter/4, inject_setter/4]).
 
 add_attribute([Class, Modifiers, Type, Name]) :-
-  add_attribute(Class, Modifiers, Type, Name, _).
+  add_attribute(Class, Modifiers, Type, Name, Attribute),
+  create_accessors(Class, Attribute, Type, Name),
+  find_owner(Attribute, File),
+  inject_attribute(File, Class, Modifiers, Type, Name),
+  inject_getter(File, Class, Modifiers, Name),
+  inject_setter(File, Class, Modifiers, Name).
 
 apply_transformation(Graph, Transformation, Arguments) :-
   string(Transformation),
