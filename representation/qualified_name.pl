639e358514af1022117bc257253dbc722362ffe0
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-05-18 23:04:15 -0300

    Adds qualified_name manipulation theorems

--- representation/qualified_name.pl
+++ representation/qualified_name.pl
@@ -1,8 +1,9 @@
 :- module(qualified_name, [
-  generate_qualified_name/3
+  generate_qualified_name/3,
+  qualified_name/3
 ]).
 
-:- use_module(text, [join_atoms/3]).
+:- use_module(text, [join_atoms/3, join_strings/3]).
 :- use_module('../model/graph', [vertex/2]).
 
 is_qualified_name_available(QualifiedName) :-
@@ -22,3 +23,13 @@ generate_qualified_name(Parent, Name, Number, QualifiedName) :-
 
 generate_qualified_name(Parent, Name, QualifiedName) :-
   generate_qualified_name(Parent, Name, 1, QualifiedName), !.
+
+qualified_name(QualifiedName, Package, Identifier) :-
+  split_string(QualifiedName, ".", "", Parts),
+  split_qualified_name(Parts, PackageParts, Identifier),
+  join_strings(PackageParts, ".", Package).
+
+split_qualified_name([], [], "").
+split_qualified_name([Part], [], Part).
+split_qualified_name([PartA|Rest], [PartA|Partial], Name) :-
+  separate_qualified_name(Rest, Partial, Name).
